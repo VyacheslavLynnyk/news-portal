cb1e216663f4aac46f7eda5bea6baf72d9490291
VyacheslavLynnyk <vyacheslav.lynnyk@gmail.com>
2016-05-30 21:30:53 +0300

    Жестяк2 Убрал выскакивающие окна

--- webroot/js/main-js.js
+++ webroot/js/main-js.js
@@ -2,16 +2,16 @@
 
 $(function() {
 
-    var closeWind = 1;
-        $('body').mouseleave(function () {
-            if (closeWind == 1) {
-                if (confirm("Close Window?")) {
-                    closeWind = 0;
-                }
-            }
-        });
-
-    $('#mail').delay(15000).fadeIn(1000);
+    // var closeWind = 1;
+    //     $('body').mouseleave(function () {
+    //         if (closeWind == 1) {
+    //             if (confirm("Close Window?")) {
+    //                 closeWind = 0;
+    //             }
+    //         }
+    //     });
+    //
+    // $('#mail').delay(15000).fadeIn(1000);
 });/**
  * Created by litter on 04.05.16.
  */
